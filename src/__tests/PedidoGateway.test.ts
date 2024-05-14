a66f47de4ac906419b1f3ce474d352e872f728bf
miltonabdon <miltonabdon69@gmail.com>
2024-05-13 22:05:11 -0300

    adicionado novos testes

--- src/__tests/PedidoGateway.test.ts
+++ src/__tests/PedidoGateway.test.ts
@@ -1,9 +1,13 @@
+import { Pedido } from "@/entities/Pedido";
+import { Produto } from "@/entities/Produto";
+import { ProdutosDoPedido } from "@/entities/ProdutosDoPedido";
+import StatusPedido from "@/entities/StatusPedido";
 import { PedidoGateway } from "@/gateways/pedido";
 import { IPedidoGateway } from "@/interfaces";
 import mockPedidoRepository from "./mocks/MockPedidoRepository";
 
 
-describe("PedidoGateway", () => {
+describe("PedidoGateway - deve retornar pedido por id", () => {
     let pedidoGateway: IPedidoGateway;
 
     beforeAll(async () => {
@@ -16,4 +20,114 @@ describe("PedidoGateway", () => {
 
         expect(pedidoBuscado).toBeDefined();
     });
+
+
 })
+
+describe("PedidoGateway - deve retornar lista de pedidos", () => {
+
+    let pedidoGateway: IPedidoGateway;
+
+    beforeAll(async () => {
+        pedidoGateway = new PedidoGateway(mockPedidoRepository);
+    })
+
+    it("get lista", async () => {
+        const pedidos: any = await pedidoGateway.getPedidos();
+
+        expect(pedidos).toBeDefined();
+
+    })
+
+})
+
+describe("PedidoGateway - deve retornar pedido por status", () => {
+    let pedidoGateway: IPedidoGateway
+
+    beforeAll(async () => {
+        pedidoGateway = new PedidoGateway(mockPedidoRepository);
+    })
+
+    it("get pedido por status", async () => {
+        const pedidos: any = await pedidoGateway.getPedidosByStatus(3);
+
+        expect(pedidos).toBeDefined();
+
+    })
+
+    
+})
+
+describe("PedidoGateway - deve retornar pedido por fake checkout", () => {
+    let pedidoGateway: IPedidoGateway
+
+    beforeAll(async () => {
+        pedidoGateway = new PedidoGateway(mockPedidoRepository);
+    })
+
+    it("get pedido por fake checkout", async () => {
+        const pedidos: any = await pedidoGateway.getPedidoByStatusFakeCheckout("Em");
+
+        expect(pedidos).toBeDefined();
+
+    })
+
+    
+})
+
+describe("PedidoGateway - deve criar pedido", () => {
+    let pedidoGateway: IPedidoGateway
+
+    const pedidoFake = criarPedidoFake();
+
+    beforeAll(async () => {
+        pedidoGateway = new PedidoGateway(mockPedidoRepository);
+    })
+
+   
+
+    it("create pedido", async () => {
+        const pedidos: any = await pedidoGateway.createPedido(pedidoFake);
+
+        expect(pedidos).toBeDefined();
+
+    })
+
+    
+})
+
+
+function criarPedidoFake(): Pedido {
+    // Dados fictícios do pedido
+    const pedido: Pedido = {
+        id: 1,
+        clienteId: 1,
+        pagamentoId: 1,
+        statusPedidoId: 1,
+        statusPedido: { id: 1, enumerador: "Em Preparação" } as StatusPedido,
+        ProdutosDoPedido: [
+            {
+                id: 1,
+                produtoId: 1,
+                produto: { id: 1, nome: "Produto 1", preco: 10 } as unknown as Produto,
+                pedidoId: 1,
+                pedido: {} as Pedido,
+                quantidade: 2,
+                valor: 20,
+            } as ProdutosDoPedido,
+            {
+                id: 2,
+                produtoId: 2,
+                produto: { id: 2, nome: "Produto 2", preco: 15 } as unknown as Produto,
+                pedidoId: 1,
+                pedido: {} as Pedido,
+                quantidade: 1,
+                valor: 15,
+            } as ProdutosDoPedido,
+        ],
+        createdAt: new Date,
+        updatedAt: new Date
+    };
+
+    return pedido;
+}
--- src/__tests/mocks/MockPedidoGateway.ts
+++ src/__tests/mocks/MockPedidoGateway.ts
@@ -9,4 +9,31 @@ jest.spyOn(mockPedidoGateway, "getPedidoById")
         return mockPedidoRepository.getPedidoById(idPedido);
     });
 
+jest.spyOn(mockPedidoGateway, "createPedido")
+    .mockImplementation(async (pedido: Pedido) => {
+        return mockPedidoRepository.create(pedido);
+    });
+
+jest.spyOn(mockPedidoGateway, "getPedidos")
+    .mockImplementation(async (pedido: Pedido[]) => {
+        return mockPedidoRepository.getPedidos(pedido);
+    });
+
+jest.spyOn(mockPedidoGateway, "getPedidosByStatus")
+    .mockImplementation(async (pedido: Pedido[]) => {
+        return mockPedidoRepository.getPedidosByStatus(pedido);
+    });
+
+jest.spyOn(mockPedidoGateway, "getPedidoByStatusFakeCheckout")
+    .mockImplementation(async (pedido: Pedido[]) => {
+        return mockPedidoRepository.getPedidoByStatusFakeCheckout(pedido);
+    });
+
+
+jest.spyOn(mockPedidoGateway, "updatePedido")
+    .mockImplementation(async (idPedido: number, statusPedido: string) => {
+        return mockPedidoRepository.updatePedido(1, "Em Preparo");
+    });
+
+
 export default mockPedidoGateway;
